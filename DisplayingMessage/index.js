ecf90dc0653cd3822d466303c63fc1485987a25d
Vaishnavi-NV <74847410+Vaishnavi-NV@users.noreply.github.com>
2021-05-18 11:50:26 +0530

    Update index.js

--- DisplayingMessage/index.js
+++ DisplayingMessage/index.js
@@ -1,1 +1,10 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './c:/Users/VAISHNAVI/demo_2/src/DisplayMessage/index1';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
