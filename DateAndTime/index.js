eee0363a2efaca993aface426ad0e1fd06504821
Vaishnavi-NV <74847410+Vaishnavi-NV@users.noreply.github.com>
2021-05-19 10:27:23 +0530

    index file

---
+++ DateAndTime/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './DateAndTime';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
